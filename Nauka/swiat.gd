5e8e861b7a5aca7f1484e363214f9c2eaa421abb
Jan Skowronski <jan@permutive.com>
2026-08-06 11:55:20 +0100

    add mouse button check

---
+++ Nauka/swiat.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+const ENEMY = preload("uid://2oqfqxx5b61g")
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == 1 and event.pressed:
+			var enemy: Node2D = ENEMY.instantiate()
+			enemy.position = event.position
+			enemy.name = "Yesod"
+			add_child(enemy)
+			print(enemy)
+			
+			
