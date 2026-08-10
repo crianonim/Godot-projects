a09b26802ca3d88c9a8673a27386cfc149f8d282
Jan Skowronski <crianonim@gmail.com>
2026-08-10 21:05:57 +0100

    player stat and pause

--- Nauka/topdown/main.gd
+++ Nauka/topdown/main.gd
@@ -3,6 +3,9 @@ class_name MainTopDown
 
 var current_scene: Node = null
 @onready var player: CharacterBody2D = $Player
+var scene_transitioning: bool = false
+var paused : bool = false
+@onready var world_scene: Node2D = $WorldScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,20 +15,25 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	debug_player_tile()
+	pass
 
 func change_scene(scene_name: String):
+	print("Change scene", scene_name)
 	var full_path = "res://topdown/%s.tscn" % scene_name
 	var new_packed_scene: PackedScene = load(full_path)
 	if new_packed_scene == null:
 		push_error("There was a problem loading %s" % full_path)
 		return
 	if current_scene:
+		print("Current scene", player)
+		add_child(player)
+		player.position=Vector2(-100,-100)
 		current_scene.remove_child(player)
 		current_scene.queue_free()
-		add_child(player)
+		
 
 	var new_scene = new_packed_scene.instantiate()
-	var player_start = new_scene.find_child("PlayerStart")
+	var player_start = new_scene.find_child("StartPosition")
 	
 	
 	
@@ -36,9 +44,34 @@ func change_scene(scene_name: String):
 	current_scene = new_scene
 	remove_child(player)
 	new_scene.add_child(player)
-	$WorldScene.add_child(new_scene)
+	world_scene.add_child(new_scene)
+	var north_exit = new_scene.find_child("ExitNorth")
+	if north_exit:
+		north_exit.connect("body_entered",on_north_exit)
+	print(north_exit)
+	scene_transitioning=false
+
+func on_north_exit(body: Node):
+	print("Body exited north",body)
+	if body == player:
+		if scene_transitioning:
+			return
+		scene_transitioning=true
+		print("Player exited")
+		change_scene("farm_1")
+		
 
 func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		print("Pressed cancel")
+		# It's the same tree, not a subtree
+		var wst=world_scene.get_tree()
+		print("Same?", wst, get_tree(), wst == get_tree())
+		# Same?<SceneTree#34208744906><SceneTree#34208744906>true
+
+		paused=!paused
+		get_tree().paused=paused
+		
 	if event is InputEventMouseButton:
 		if event.pressed:
 			var epos: Vector2 = event.position
@@ -59,6 +92,7 @@ func _input(event: InputEvent) -> void:
 			#print(player.position)
 			
 func debug_player_tile() -> void:
+	#print(player)
 	var tm: TileMapLayer=current_scene.find_child("TileMapLayer")
 	var clicked_cell: Vector2i = tm.local_to_map(player.position)
 	var tile = tm.get_cell_atlas_coords(clicked_cell)
--- Nauka/topdown/player.gd
+++ Nauka/topdown/player.gd
@@ -3,15 +3,15 @@ extends CharacterBody2D
 
 const SPEED = 50.0
 
-
+var survived : float = 0
 
 func _physics_process(delta: float) -> void:
 	
-
+	survived = survived + delta
 	
 	var input_direction := Input.get_vector("ui_left", "ui_right", "ui_up","ui_down")
 	velocity = input_direction * SPEED
 	#if input_direction != Vector2.ZERO:
 		#print("Vel:",velocity)
-
+	$Label.text=str(int(survived))
 	move_and_slide()
