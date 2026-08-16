43076d7730a4fe2e6aea612eae060e097462d8e4
Jan Skowronski <crianonim@gmail.com>
2026-08-16 16:36:59 +0100

    simple expression to check if show

--- Nauka/dialog/dialog_option.gd
+++ Nauka/dialog/dialog_option.gd
@@ -3,4 +3,17 @@ class_name DialogOption
 
 @export var text: String = "Option text"
 @export var target: String = "#end"
+@export var show_if_text: String = "true":
+	set(val):
+		show_if_text=val
+		#var expression : Expression = Expression.new()
+		#
+		#print("Given", val)
+		#var error = expression.parse(val,["state"])
+		#if error != OK:
+			#print("ERROR:")
+			#print(expression.get_error_text())
+		#else:
+			#var result = expression.execute([{"flags":{"met_bob":true}}])
+			#print("Result ",result)
 	
--- Nauka/dialog/dialog_ui.gd
+++ Nauka/dialog/dialog_ui.gd
@@ -3,6 +3,17 @@ extends Control
 @onready var options_container: VBoxContainer = $Panel/MarginContainer/VBoxContainer/OptionsContainer
 
 @export var dialogs: DialogTree
+var expression : Expression = Expression.new()
+
+var state : Dictionary = { 
+	"flags": {
+		"met_bob": true,
+		"offended_kate": false
+		
+	},
+	"money" : 100,
+	"name": "Jan"
+}
 signal finished
 var current_dialog : Dialog
 # Called when the node enters the scene tree for the first time.
@@ -20,18 +31,38 @@ func setup_dialog(dialog: Dialog) -> void:
 	for child in options_container.get_children():
 		child.queue_free()
 	for option in dialog.options:
+		var can_show = can_show_option(option)
+		print("Can show? '", option.show_if_text, "' :", can_show)
+		if not can_show:
+			continue
 		var optionLabel = Button.new()
 		optionLabel.text = option.text
 		print(option.target)
 		optionLabel.pressed.connect(on_button_pressed_option.bind(option.target))
 		options_container.add_child(optionLabel)
 		
-		
+
 func on_button_pressed_option(target: String) -> void:
 	print("Selected", target)
 	if target == DialogTree.END_TARGET:
 		finished.emit()
+		# Close for now
+		get_tree().quit() 
 		return
 	
 	current_dialog = dialogs.dialogs.get(target)
 	setup_dialog(current_dialog)
+
+func can_show_option(option: DialogOption) -> bool:
+	if option.show_if_text==null:
+		return true
+	else:
+		var error = expression.parse(option.show_if_text,["state"])
+		if error != OK:
+			print("ERROR:")
+			print(expression.get_error_text())
+			return false
+		else:
+			var result = expression.execute([state])
+			print("Result ",result)
+			return result
